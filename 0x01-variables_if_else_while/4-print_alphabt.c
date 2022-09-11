579af0a9ce46d96c529541331651937965e49f9d
SOFT <ayobamifridaus@gmail.com>
2022-09-11 06:18:28 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	char alp;
 
-	for (alp = 'a'; alp <= 'z' && alp != 'q' && alp != 'e' ;  alp++)
+	for (alp = 'a'; alp <= 'z' && (alp != 'q' && alp != 'e') ;  alp++)
 	{
 	putchar(alp);
 	}
