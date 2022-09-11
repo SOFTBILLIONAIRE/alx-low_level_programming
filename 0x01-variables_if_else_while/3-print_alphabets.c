4c8868926f1653c962557b549c5e23689ea5dad6
SOFT <ayobamifridaus@gmail.com>
2022-09-11 04:44:44 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Allow success
+*/
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		putchar(alp);
+	}
+			for (alp = 'A'; alp <= 'Z'; alp++)
+			{
+				putchar(alp);
+			}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
+
