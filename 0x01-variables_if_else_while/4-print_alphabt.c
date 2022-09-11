f816d5f6965c1b8fd0aced47329ea5adb33e19b5
SOFT <ayobamifridaus@gmail.com>
2022-09-11 06:12:50 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Allow success
+*/
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z' && alp != 'q' && alp != 'e' ;  alp++)
+	{
+	putchar(alp);
+	}
+return (0);
+}
