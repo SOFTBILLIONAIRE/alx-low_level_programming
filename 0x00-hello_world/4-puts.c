f11eb1a30ed694d2e7bd025f3cc3d3c6fe00e167
SOFT <ayobamifridaus@gmail.com>
2022-09-12 14:29:08 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Allow success
+*/
+int main(void)
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
