021cb6011b7d508633a73ad3cee981a68ead2a86
SOFT <ayobamifridaus@gmail.com>
2022-09-12 11:53:02 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Allow sucess
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long c;
+	long long d;
+	float e;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(a));
+		printf("Size of a int: %zu byte(s)\n", sizeof(b));
+		printf("Size of a long: %zu byte(s)\n", sizeof(c));
+		printf("Size of a long long: %zu byte(s)\n", sizeof(d));
+		printf("Size of a float: %zu byte(s)\n", sizeof(e));
+		      }
