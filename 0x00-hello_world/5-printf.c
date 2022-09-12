77d84f362ea643034050f2a30a978f3e301b7feb
SOFT <ayobamifridaus@gmail.com>
2022-09-12 11:24:44 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-printf("with proper grammar, but the outcome is a piece of art,");
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Allow success
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
