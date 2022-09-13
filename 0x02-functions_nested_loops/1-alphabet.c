74444e569063184c3ce2058977257f681c9ed5f6
SOFT <ayobamifridaus@gmail.com>
2022-09-13 10:58:23 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Allow success
+*/
+int main(void)
+{
+	char b;
+
+	b = 'a';
+	while (b <= 'z')
+	{
+		putchar(b);
+		b++;
+	}
+return (0);
+}
