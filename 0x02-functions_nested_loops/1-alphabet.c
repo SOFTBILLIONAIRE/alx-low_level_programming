97dfd1da2b519c65012d7cb7f1484e49305169f3
SOFT <ayobamifridaus@gmail.com>
2022-09-13 12:10:14 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  *
  * Return: Allow success
 */
 void print_alphabet(void)
 {
 	char alp;
-	
+
 	alp = 'a';
 	while (alp <= 'z')
-	{ 
+	{
 		_putchar(alp);
 		alp++;
 	}
