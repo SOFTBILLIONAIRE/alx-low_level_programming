9041964d6f8bbc701bf037a263f9b18a3ef2e2a3
SOFT <ayobamifridaus@gmail.com>
2022-09-13 09:22:20 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,6 +6,42 @@
 */
 int main(void)
 {
-	puts("_putchar\n");
+	char a;
+
+	a = '_';
+	{
+		putchar(a);
+	}
+	a = 'p';
+	{
+		putchar(a);
+	}
+	a = 'u';
+	{
+		putchar(a);
+	}
+	a = 't';
+	{
+		putchar(a);
+	}
+	a = 'c';
+	{
+		putchar(a);
+	}
+	a = 'h';
+	{
+		putchar(a);
+	}
+	a =  'a';
+	{
+		putchar(a);
+	}
+	a = 'r';
+	{
+		putchar(a);
+	}
+	{
+		putchar('\n');
+	}
 return (0);
 }
