c42f4cd10d8eb9073f82bcabb2f9ca20575bda43
SOFT <ayobamifridaus@gmail.com>
2022-09-13 15:05:48 -0700

    int _islower(int c);

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  _islower - Entry point
+ *
+ * Return: Allow success
+*/
+int _islower(int c)
+{
+	int c;
+
+	if (c >= 0 && c <= 177)
+	{
+		_putchar(int _islower(int c));
+	}
+
+
