37629c43e1e14e2388cdf10b7ec0dc1f3f7214fb
SOFT <ayobamifridaus@gmail.com>
2022-09-14 14:06:47 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * jack_bauer - a function that prints every minute of the day of Jack.
+ * n from 00:00 to 23:59
+ * Return: Allow success
+*/
+void jack_bauer(void)
+{
+	int h = 0;
+	int m = 0;
+	int hr;
+	int mr;
+
+	while (h <= 23)
+	{
+		while (m <= 59)
+		{
+			hr = h % 10;
+			_putchar(h / 10 + '0');
+			_putchar(hr + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar(mr + '10');
+			m++;
+			_putchar('\n');
+		}
+		h=++;
+		m = 0;
+	}
+}
+
