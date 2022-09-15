6f4551a708378a0a50f6ecec5c09e05f5ae9e612
SOFT <ayobamifridaus@gmail.com>
2022-09-15 15:23:15 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * int mul - a function that multiplies twu numbers
+ * @a: defines the parameter list for the function
+ * @b: defines the parameter list for the function
+ * Return: Allow success
+*/
+int mul(int a, int b)
+{
+	int n;
+
+	n = a * b;
+
+	return (n);
+}
