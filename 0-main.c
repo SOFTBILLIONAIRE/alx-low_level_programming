3a7e042ff02704a9c4004bce8394ca3e346b8314
SOFT <ayobamifridaus@gmail.com>
2022-09-15 04:24:09 -0700

    Multiple mains

---
+++ 0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * positive_or_negative - A function that prints if integer is negative or pos.
+ *
+ * Return: Allow success
+*/
+int main(void)
+{
+	        int i;
+
+		        i = 0;
+			        positive_or_negative(i);
+
+				        return (0);
+}
---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Allow success
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015\-10\-19\n",59)
+	return (1);
