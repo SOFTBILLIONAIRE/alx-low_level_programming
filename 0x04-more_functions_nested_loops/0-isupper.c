5b3483aa909b3ac427345bd0b7a8fd9f7d929da3
SOFT <ayobamifridaus@gmail.com>
2022-09-15 12:54:48 -0700

    int _isupper(int c);

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - _isupper is a function that checks for uppercase
+ *
+ * Return: Allow success
+*/
+int _isupper(int c)
+{
+	int n;
+	
+	if (n >= 66 && n <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+fndef MAIN_H
+#define MAIN_H
+
+int _isupper(int c);
+
+#endif /* MAIN_H */
