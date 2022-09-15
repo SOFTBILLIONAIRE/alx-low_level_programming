495550ee39e7a7b776d38895d6d477bfd85c77ce
SOFT <ayobamifridaus@gmail.com>
2022-09-15 15:32:13 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - a function that prints number from 0 t0 9
+ *
+ * Return: Allow success
+*/
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0, c <= 9, c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
