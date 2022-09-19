de5123fd288b13c8bc58715aa9aa43484e436c6d
SOFT <ayobamifridaus@gmail.com>
2022-09-19 13:20:52 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,10 +3,10 @@
  * swap_int - A function that swap the value of two integers
  * @a: define the first parameter
  * @b: define the second parameter
- * Return: Allo success
+ * Return: Allow success
 */
 void swap_int(int *a, int *b)
-{	
+{
 	int p;
 
 	p = *a;
