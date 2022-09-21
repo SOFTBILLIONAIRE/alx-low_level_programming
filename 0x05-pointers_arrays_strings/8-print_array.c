50b034f47a0ae659a49140deca558e83d31a52ce
SOFT <ayobamifridaus@gmail.com>
2022-09-20 23:58:28 -0700

    void print_array(int *a, int n);

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_array - a function that prints n elements of an array of integers
+ * @a: defines the parameter for the function
+ * @n: defines the parameter for the function
+*/
+void print_array(int *a, int n);
+{
+	n = 0;
+	while (a[n] != '\0')
+	{
+		printf("%d", a[0]);
+	}
+	_putchar('\n');
+}
