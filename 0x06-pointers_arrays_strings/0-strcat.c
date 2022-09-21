f80a9e69595f7f9d9a89705c9ab86009de7e61b6
SOFT <ayobamifridaus@gmail.com>
2022-09-21 16:55:07 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,9 @@ char *_strcat(char *dest, char *src)
 	char dest_index, src_index, i;
 
 	for (dest_index = 0; dest[dest_index] != '\0'; dest_index++)
-	{
-		dest_index;
-	}
+	;
 	for (src_index = 0; src[src_index] != '\0'; src_index++)
-	{
-		src_index;
-	}
+	;
 	for (i = 0; i < src_index; i++)
 	{
 		dest[dest_index] = src[i];
