006c0abc752342b8e2302f87392c793ad04dea43
SOFT <ayobamifridaus@gmail.com>
2022-09-23 16:06:38 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - encodes a string in 1337
+ * @s: string to be encoded
+ * Return: Allow success
+*/
+char *leet(char *s)
+{
+	int i, j;
+	char *a = "aAeEo0tTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if(s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+	return (s);
+}
+
