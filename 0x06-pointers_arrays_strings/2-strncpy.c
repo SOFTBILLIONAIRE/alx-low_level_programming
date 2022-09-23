4c88778162c338207d1a2706ba32d3e4a07200ec
SOFT <ayobamifridaus@gmail.com>
2022-09-23 13:50:32 -0700

    char *_strncpy(char *dest, char *src, int n)

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncpy - a function that copies src to dest
+ * @dest: first parameter
+ * @src: second parameter
+ * @n: number of byte
+ * Return: Allow success
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n && src[i] == '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
