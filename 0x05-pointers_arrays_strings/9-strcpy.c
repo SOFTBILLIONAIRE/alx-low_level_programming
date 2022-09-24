e290ce48e8ace05213086dbac61334ec8d67f7c4
SOFT <ayobamifridaus@gmail.com>
2022-09-23 18:23:30 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest - first parameter
+ * @src: second parameter
+ * Return: Allow success
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+	return (dest);
+}
+
