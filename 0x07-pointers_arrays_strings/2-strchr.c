227ecd0d6f79e4b23ddea04d587bf13fb99538ff
SOFT <ayobamifridaus@gmail.com>
2022-09-26 23:43:56 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,13 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	for (i = 0; ; i++)
 		{
 			if (*(s + i) == c)
 			{
 			return (s - 1);
 			}
-			else
+			if (*(s + i) == 0)
 			{
 			return (NULL);
 			}
