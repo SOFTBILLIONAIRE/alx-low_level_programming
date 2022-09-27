dafa0b2ffb2a2690e8c08235af5e03d31894e0a5
SOFT <ayobamifridaus@gmail.com>
2022-09-26 22:32:20 -0700

    char *_memcpy(char *dest, char *src, unsigned int n)

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memcpy - a function that copies memory area.
+ * @dest: first parameter
+ * @src: second parameter
+ * @n: third parameter
+ * Return: Allow success
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+
+}
