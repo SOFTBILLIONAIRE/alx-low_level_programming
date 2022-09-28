482cd20669b2176d11cbb838cb14db853de57cd0
SOFT <ayobamifridaus@gmail.com>
2022-09-28 09:51:25 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,15 +12,18 @@ unsigned int _strspn(char *s, char *accept)
 	len = 1;
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j]; j++)
 		{
 		if (s[i] == accept[j])
 		{
 			c = len++;
 			break;
 		}
-		}
+		else if (s[i] == '\0')
+		{
 		return (len);
+		}
+		}
 	}
 	return (c);
 }
