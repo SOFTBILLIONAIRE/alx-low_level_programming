23c5b51122b0600dac98ef931cf17ad77cef1866
SOFTBILLIONAIRE <ayobamifridaus@gmail>
2022-09-29 17:52:00 +0100

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,16 @@
 */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	int i = 0;
+
+
+	if (*(s + i) == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	_putchar(*(s + i));
+	i++;
+	_puts_recursion(s + i);
 return;
 }
