effa726e3c2c81a4d4876b215e610443fcb9ac06
SOFTBILLIONAIRE <ayobamifridaus@gmail>
2022-10-01 09:02:52 +0100

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - A function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Allow succes
+*/
+int main(int argc, char *arg[v])
+{
+	printf("%d\n", argc);
+return (0);
+}
