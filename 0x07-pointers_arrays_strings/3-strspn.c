ee424d055258fde451d741c45f817e89fd086415
SOFT <ayobamifridaus@gmail.com>
2022-10-01 22:49:34 +0100

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,11 +12,11 @@ unsigned int _strspn(char *s, char *accept)
 	len = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = 0; accept[i] != '\0'; i++)
+		for (i = 0; accept[i]; i++)
 		{
 		if (s[i] == accept[i])
 		{
-			len++;  
+			len++;
 			break;
 		}
 		else if (accept[i + 1] == '\0')
