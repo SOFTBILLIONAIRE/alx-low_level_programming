d6f42c296e0ed80c7debb2074c3ebfffcb2e1ce6
SOFTBILLIONAIRE <ayobamifridaus@gmail>
2022-10-01 06:48:06 +0100

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,7 @@
-#include "main.h"
+#include "stdio.h"
 
 int main(int argc, char* argv[])
 {
-	while (*argv[0] != '\0')
-	{
-		argv[0]++;
-		_putchar(*argv[0]);
-	}
-
+	printf("%s", argv[0]);
 return (0);
 }
