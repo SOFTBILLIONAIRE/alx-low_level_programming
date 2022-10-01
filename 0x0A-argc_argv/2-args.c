9af4d74c9ebb1e0d2923901f0f3830e4c2f6e521
SOFT <ayobamifridaus@gmail.com>
2022-10-01 06:38:13 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,10 +7,12 @@
 */
 int main(int argc, char *argv[])
 {
-	while(*argv != argv[argc])
+	int i = 0;
+
+	while (i < argc)
 	{
-		printf("%s\n", *argv);
-			argv++;
+		printf("%s\n", argv[i]);
+			i++;
 	}
 return (0);
 }
