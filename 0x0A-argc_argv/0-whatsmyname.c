e03358fcfb498a492b3c7cd33845c6ad8020dc66
SOFTBILLIONAIRE <ayobamifridaus@gmail>
2022-10-01 07:31:12 +0100

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,11 @@
 #include "stdio.h"
-
+/**
+ * main - first function
+ * @argc: first parameter
+ * @argv: second parameter
+*/
 int main(int argc, char* argv[])
 {
 	printf("%s\n", argv[0]);
-	(void)argc;
 return (0);
 }
