a7f5e96c594973ae96fbf92013203e399a430192
SOFT <ayobamifridaus@gmail.com>
2022-10-04 21:34:40 +0000

    char *1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strdup - a function 
+ * @str: parameter for the function
+ * Return: Allow success
+*/
+char *_strdup(char *str)
+{
+	char *p;
+	int i = 0, count = 0;
+
+	while (*(str + i) != '\0')
+	{
+		count++;
+		i++;
+	}
+	p = malloc(i * sizeof(char))
+		if (str == NULL)
+		{
+			return ('\0');
+		}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		p[i] = str[i];
+	}
+	return (p);
+}
