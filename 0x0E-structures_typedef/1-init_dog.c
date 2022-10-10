cdde8814628d373d5cf7d95163830cd44642abb0
SOFT <ayobamifridaus@gmail.com>
2022-10-10 20:58:32 +0000

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,11 +8,10 @@
  * @owner: fourth parameter
  * Return: Allow success
 */
-init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d = malloc(sizeof(init_dog));
 			d->name = name;
 			d->age = age;
 			d->owner = owner;
-return (d);
 }
