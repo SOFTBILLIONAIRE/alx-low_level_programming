a449957838b3f5bdbb95e50c3144b054fc0354d8
SOFT <ayobamifridaus@gmail.com>
2022-10-10 20:12:50 +0000

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+A task that test our knowledge on struct and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - a user define data type
+ *
+ * Return: Allow success
+*/
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
