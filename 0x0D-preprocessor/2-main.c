6ebb7cdf3a70870048dc07cd9538932e93ff1559
SOFT <ayobamifridaus@gmail.com>
2022-10-11 23:23:24 +0000

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int _putchar(char c);
+#define NAME(*s) (while (*s != '\0'))/
+			{/
+				_putchar(*s);/
+				s++;/
+			}
+/**
+ * main -
+ * Return: Allow success
+*/
+int main(void)
+{
+	NAME("2-main.c");
+return (0);
+}
