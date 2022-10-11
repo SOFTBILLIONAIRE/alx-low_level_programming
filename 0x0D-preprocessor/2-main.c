ace6a43fa522a85035f6dbfbd208caeaa465315c
SOFT <ayobamifridaus@gmail.com>
2022-10-11 23:32:41 +0000

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-int _putchar(char c);
-#define NAME(*s) (while (*s != '\0'))/
-			{/
-				_putchar(*s);/
-				s++;/
-			}
 /**
  * main -
  * Return: Allow success
 */
 int main(void)
 {
-	NAME("2-main.c");
+	printf("%s", _FILE_);
 return (0);
 }
