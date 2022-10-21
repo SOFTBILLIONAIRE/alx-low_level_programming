5a5df1bb0b3abf0674ffb3aa97671252e8449e5a
SOFT <ayobamifridaus@gmail.com>
2022-10-21 09:13:37 +0100

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,19 +3,19 @@
 
 #include <stdlib.h>
 /**
- * struct list - A new data type
- * @str: first parameter
- * @len: second parameter
- * @next: third parameter
- * Return: Allow success
- * Description: singly linked node structure
-*/
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
-	char *str;
-	unsigned len;
-	struct list *next;
-}list_t;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
