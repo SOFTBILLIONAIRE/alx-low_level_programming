0b1fde105357282bde5c501193a82c6667a8e43f
SOFT <ayobamifridaus@gmail.com>
2022-10-21 10:53:04 +0100

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,3 +1,19 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
+/**
+ * list_len - A function that returns no of element.
+ * @h: A pointer to the address of the first node
+ * Return: Allow success
+*/
+size_t list_len(const list_t *h)
+{
+	int count = 0;
 
+	while(h)
+	{
+		count++;
+		h = h->next;
+	}
+return (count);
+}
