44a5c52435ee99391c7d1795cd7b3fe69e98ec86
SOFT <ayobamifridaus@gmail.com>
2022-10-23 21:54:15 +0100

    0-print_list.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+/**
+ * add_node - A function that add a new string.
+ * @h: first parameter.
+ * @str: new string.
+ * Return: Allow success
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	 
+}
