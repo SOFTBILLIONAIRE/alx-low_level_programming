cefb8fc369e49c79fb9d051faba20dbe5680da0f
SOFT <ayobamifridaus@gmail.com>
2022-10-23 23:49:00 +0100

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,21 +4,25 @@
 #include <string.h>
 /**
  * add_node - A function that add a new string.
- * @h: first parameter.
+ * @head: first parameter.
  * @str: new string.
  * Return: Allow success
 */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new;
+	new = malloc(sizeof(list_t));
+	*head = new;
 	int count = 0;
-	list_t  *new_node;
-	new_node = malloc(sizeof(list_t));
-	head = &new_node;
-	new_node->str = strdup(str);
+
+	if (new == NULL)
+		return (NULL);
 	while (*str)
 	{
 		count++;
 	}
-	new_node->len = count;
-return (new_node);
+	new->str = strdup(str);
+	new->len = count;
+	new->next = *head;
+return (new);
 }
